975fd9203550dc73d6f64ca6775ff21524ca1853
Kayronny123 <kayronnysales@gmail.com>
2023-06-02 18:08:46 -0300

    Iniciando modelo de arquitetura de users e posts

---
+++ src/database/PostDatabase.ts
@@ -0,0 +1,7 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class PostDatabase extends BaseDatabase{
+    public static TABLE_POST = "post"
+    public static TABLE_LIKES_DISLIKES = "likes_dislikes"
+    // metodos DB
+}
