ff27d11fcfe70ba1045a45f2da7ceb005d565d4f
Kayronny123 <kayronnysales@gmail.com>
2023-06-03 12:15:46 -0300

    Criando métodos de login de usuario

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,8 +1,9 @@
 import { UserBusiness } from "../business/UserBusiness";
 import {ZodError} from "zod";
-import {Request, Response} from "express";
+import {Request, Response, response} from "express";
 import { BaseError } from "../errors/BaseError";
 import { SignupSchema } from "../dtos/user/signup.dto";
+import { LoginSchema } from "../dtos/user/login.dto";
 
 export class UserController {
     constructor (
@@ -19,6 +20,27 @@ public signup = async (req: Request, res: Response) =>{
         const output = await this.userBusiness.signup(input);
         res.status(201).send(output);
 
+
+    } catch (error) {
+        console.log(error);
+        if(error instanceof ZodError){
+            res.status(400).send(error.issues)
+        } else if (error instanceof BaseError){
+            res.status(error.statusCode).send(error.message)
+        } else {
+            res.status(500).send("Error inesperado")
+        }
+    }
+}
+public login = async (req: Request, res:Response)=>{
+    try {
+    const input = LoginSchema.parse({
+        email: req.body.name,
+        password: req.body.passowrd
+    })       
+
+    const output = await this.userBusiness.login(input);
+    res.status(200).send(output)
 
     } catch (error) {
         console.log(error);
