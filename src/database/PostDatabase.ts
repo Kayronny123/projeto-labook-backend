c101157fb260f970160c382e41f73f4cac3297d2
Kayronny123 <kayronnysales@gmail.com>
2023-06-06 21:06:57 -0300

    Criando metodos de deletar posts

--- src/database/PostDatabase.ts
+++ src/database/PostDatabase.ts
@@ -52,4 +52,13 @@ export class PostDatabase extends BaseDatabase {
             .insert(postDB)
             .where({id: postDB.id})
         }
+        public deletePostById = async (
+            id: string
+        ):Promise <void> =>{
+            await BaseDatabase
+            .connection(PostDatabase.TABLE_POST)
+            .delete()
+            .where({id})
+
+        }
 }
